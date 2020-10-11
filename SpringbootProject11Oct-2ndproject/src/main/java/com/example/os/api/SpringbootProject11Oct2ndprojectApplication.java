69ca2d8e7e43c16c652252c16f75d0d767f21abb
venka <venka@DESKTOP-RLD91KR>
2020-10-11 18:38:17 +0530

    1st commit

---
+++ SpringbootProject11Oct-2ndproject/src/main/java/com/example/os/api/SpringbootProject11Oct2ndprojectApplication.java
@@ -0,0 +1,22 @@
+package com.example.os.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootProject11Oct2ndprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject11Oct2ndprojectApplication.class, args);
+	}
+	
+	@GetMapping("/method1")
+	public String method1() {
+		
+		return "method1 result in spring boot project";
+	}
+
+}
---
+++ SpringbootProject11Oct-2ndproject/src/test/java/com/example/os/api/SpringbootProject11Oct2ndprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.os.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject11Oct2ndprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
